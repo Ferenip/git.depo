5066a06ba79448f79d1a0501a8315c954b8d369f
fatih eren <fatiherenipek15@gmail.com>
2026-06-02 20:00:41 +0300

    Delete config.php

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Bu dosyaya doğrudan erişimi engelle
-if (basename(__FILE__) == basename($_SERVER['SCRIPT_FILENAME'])) {
-    die('Bu sayfaya doğrudan erişim engellenmiştir.');
-}
-
-// Yapay Zeka API Anahtarı (Bu dosyayı .gitignore'a eklemeyi unutmayın!)
-define('GROQ_API_KEY', '');//apı key buraya girilecek
-?>
